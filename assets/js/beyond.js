876de30e7e86b51fa0e8af3ea7bfbbcdd3b9a29a
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2021-04-21 13:05:48 -0400

    redoing About Me sidebar

--- assets/js/beyond.js
+++ assets/js/beyond.js
@@ -2,6 +2,6 @@ const elementsList = document.querySelectorAll("#randomNum");
 const elementsArray = [...elementsList];
 setInterval(function() {
     elementsArray.forEach(element => {
-        element.innerHTML = '#' + Math.floor((Math.random() * 8) + 2);
+        element.innerHTML = '#' + Math.floor((Math.random() * 8) + 2) + ":&nbsp;";
     });
 }, 10);
